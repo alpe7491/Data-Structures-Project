4500313c97aa4b1bff0e0662da92d31298afb9fd
AnthonyC856 <49504955+AnthonyC856@users.noreply.github.com>
2019-04-16 15:56:19 -0600

    Update PriorityQueue.hpp

--- PriorityQueue.hpp
+++ PriorityQueue.hpp
@@ -1,6 +1,7 @@
 #ifndef PRIORITYQUEUE_HPP
 #define PRIORITYQUEUE_HPP
 
+#include "Time.hpp"
 #include <string>
 #include <cstring>
 
@@ -9,9 +10,7 @@ struct GroupNode
 {
   std::string groupName;
   int groupSize;
-  int cookingTime;
-  int arrival time;
-  string order[groupSize];
+  Time arrivalTime;
 };
 
 
@@ -23,11 +22,13 @@ class PriorityQueue
     // Purpose: free all resources that the object has acquired
     ~PriorityQueue();
     // Purpose: enqueue new group into priority queue; call other
-    void enqueue (std::string _groupName, int _groupSize, int _cookingTime);
+    void enqueue (std::string _groupName, int _groupSize, Time _arrivalTime, bool reservation);
     //Purpose: remove the group (allocate the table) at the front of the priority queue from
     void dequeue();
+    // Purpose: remove all groups from the que;
+    void clearQue();
     // return: groupID of the group at the front of the priority queue
-    GroupNode peek();
+    GroupNode *peek();
     //return: true if queue is full, false otherwise
     bool isFull();
     // return: true if queue is empty, false otherwise
@@ -38,12 +39,10 @@ class PriorityQueue
     void repairUpward(int nodeIndex);
     // Purpose: maintain heap properties by swapping node with child if necessary
     void repairDownward(int nodeIndex);
-    // Purpose: calculate how long a parties order will take to cook 
-    int calcCookTime(string order[]);
 
-    int maxQueueSize; //maximum capacity of priority queue
-    int currentQueueSize; //number of group currently in the priority queue
     GroupNode* priorityQueue; //pointer to the array used to implement priority queue
+    int currentQueueSize; //number of group currently in the priority queue
+    int maxQueueSize; //maximum capacity of priority queue
 };
 
 #endif
